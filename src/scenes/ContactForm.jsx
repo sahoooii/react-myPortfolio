d66960e0a7158a67bdf61141d485771beee893ba
sahoooii <saho.chu@gmail.com>
2023-02-17 18:01:24 +0900

    add animation all the part

--- src/scenes/ContactForm.jsx
+++ src/scenes/ContactForm.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import { useForm } from 'react-hook-form';
 
 import LineGradient from '../components/LineGradient';
 
 const ContactForm = () => {
+	const {
+		register,
+		trigger,
+		formState: { errors },
+	} = useForm();
+
+	const onSubmit = async (e) => {
+		const isValid = await trigger();
+		if (!isValid) {
+			e.preventDefault();
+		}
+	};
+
 	return (
-		<section id='contact' className='py-48'>
+		<section id='contact' className='sm:py-48 py-24'>
 			{/* Heading */}
 			<motion.div
 				className='md:w-1/2 m-auto text-center'
@@ -15,7 +29,7 @@ const ContactForm = () => {
 				transition={{ duration: 1 }}
 				variants={{
 					hidden: { opacity: 0, y: -50 },
-					visible: { opacity: 1, x: 0 },
+					visible: { opacity: 1, y: 0 },
 				}}
 			>
 				<div>
@@ -29,20 +43,37 @@ const ContactForm = () => {
 			</motion.div>
 
 			{/* form */}
-			<div className='border-2 border-slate-500 md:w-[750px] sm:w-[600px] w-[300px] mx-auto p-10'>
-				<h1 className='font-montserrat sm:text-4xl text-2xl text-center sm:pb-6 pb-2'>
+			<motion.div
+				className='mt-20 border-2 border-slate-500 w-[85%] max-w-[750px] mx-auto p-10'
+				initial='hidden'
+				whileInView='visible'
+				viewport={{ once: true, amount: 0.5 }}
+				transition={{ delay: 0.4, duration: 1 }}
+				variants={{
+					hidden: { opacity: 0, y: -50 },
+					visible: { opacity: 1, y: 0 },
+				}}
+			>
+				{/* <div className='border-2 border-slate-500 w-[85%] max-w-[750px] mx-auto p-10'> */}
+				<h1 className='font-montserrat sm:text-4xl text-2xl text-center sm:pt-10 pt-5 sm:pb-6 pb-2'>
 					&bull; Keep in Touch &bull;
 				</h1>
 				<div className='border-b-2 border-slate-500 w-[80px] mx-auto'></div>
-				<div className='mt-4 flex items-center justify-center'>
+				<div className='sm:mt-10 mt-5 flex items-center justify-center'>
 					<img
 						src='../assets/phone-icon.png'
 						alt='phone-icon'
-						className='sm:w-[80px] w-[60px] hover:scale-75 duration-300'
+						className='sm:w-[80px] w-[60px] animate-heartbeat'
 					/>
 				</div>
 
-				<form action='#' method='post' className='mt-10'>
+				<form
+					target='_blank'
+					onSubmit={onSubmit}
+					action='https://formsubmit.co/d8ba071863b209c206352f351fbccf9e'
+					method='POST'
+					className='mt-10'
+				>
 					<div className='md:grid md:grid-cols-2 grid gap-y-6'>
 						<div className='border-b-2 border-slate-500 mx-auto md:w-[80%] sm:w-[90%] w-full'>
 							<label for='name'></label>
@@ -51,18 +82,38 @@ const ContactForm = () => {
 								placeholder='My name is'
 								name='name'
 								className='dark:bg-bluish-black bg-whitish p-2 outline-none font-opensans  placeholder:uppercase'
-								required
+								{...register('name', {
+									required: true,
+									maxLength: 100,
+								})}
 							/>
+							{errors.name && (
+								<p className='text-red-600 mt-1'>
+									{errors.name.type === 'required' && 'This field is required.'}
+									{errors.name.type === 'maxLength' &&
+										'Max length is 100 char.'}
+								</p>
+							)}
 						</div>
 						<div className='border-b-2 border-slate-500 md:w-[80%] sm:w-[90%] w-full mx-auto'>
-							<label for='name'></label>
+							<label for='phone'></label>
 							<input
 								type='text'
 								placeholder='My number is'
-								name='telephone'
+								name='phone'
 								className='dark:bg-bluish-black bg-whitish p-2 outline-none font-opensans placeholder:uppercase'
-								required
+								{...register('phone', {
+									required: true,
+									pattern: /^0\d{9,10}$/,
+								})}
 							/>
+							{errors.phone && (
+								<p className='text-red-600 mt-1'>
+									{errors.phone.type === 'required' &&
+										'This field is required.'}
+									{errors.phone.type === 'pattern' && 'Invalid phone number.'}
+								</p>
+							)}
 						</div>
 					</div>
 
@@ -74,8 +125,18 @@ const ContactForm = () => {
 								placeholder='My e-mail is'
 								name='email'
 								className='dark:bg-bluish-black bg-whitish p-2 outline-none font-opensans placeholder:uppercase'
-								required
+								{...register('email', {
+									required: true,
+									pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+								})}
 							/>
+							{errors.email && (
+								<p className='text-red-600 mt-1'>
+									{errors.email.type === 'required' &&
+										'This field is required.'}
+									{errors.email.type === 'pattern' && 'Invalid email address.'}
+								</p>
+							)}
 						</div>
 
 						<div className='mt-6 border-b-2 border-slate-500 sm:w-[90%] w-full mx-auto'>
@@ -84,15 +145,25 @@ const ContactForm = () => {
 								className='dark:bg-bluish-black bg-whitish p-2 outline-none w-[95%] text-slate-400 font-opensans placeholder:capitalize'
 								placeholder='Subject line'
 								name='subject'
-								required
+								{...register('subject', {
+									required: true,
+								})}
 							>
-								<option disabled hidden selected>
+								<option disabled hidden selected value=''>
 									SUBJECT LINE
 								</option>
-								<option>I'd like to offer you a job</option>
-								<option>I'd like to ask a question</option>
-								<option>I'd like to have a casual interview with you.</option>
+								<option value='offer'>I'd like to offer you a job</option>
+								<option value='question'>I'd like to ask a question</option>
+								<option value='interview'>
+									I'd like to have a casual interview with you
+								</option>
 							</select>
+							{errors.subject && (
+								<p className='text-red-600 mt-1'>
+									{errors.subject.type === 'required' &&
+										'This field is required.'}
+								</p>
+							)}
 						</div>
 
 						<div className='mt-6 border-b-2 border-slate-500 sm:w-[90%] w-full mx-auto'>
@@ -101,22 +172,34 @@ const ContactForm = () => {
 								name='message'
 								placeholder="I'd like to chat about"
 								className='dark:bg-bluish-black bg-whitish p-2 outline-none w-full font-opensans placeholder:uppercase'
-								cols='30'
+								cols='50'
 								rows='5'
-								required
+								{...register('message', {
+									required: true,
+									maxLength: 5000,
+								})}
 							></textarea>
+							{errors.message && (
+								<p className='text-red-600 mt-1'>
+									{errors.message.type === 'required' &&
+										'This field is required.'}
+									{errors.message.type === 'maxLength' &&
+										'Max length is 5000 char.'}
+								</p>
+							)}
 						</div>
 					</div>
 
-					<div className='m-12 rounded-sm md:w-[45%] w-full mx-auto bg-gradient-purple-pink-orange p-0.5'>
+					<div className='m-12 rounded-sm md:w-[45%] w-full mx-auto bg-gradient-purple-pink-orange p-0.5 hover:translate-y-0.5 transition duration-500 group'>
 						<input
 							type='submit'
 							value='SEND MESSAGE'
-							className='dark:bg-bluish-black bg-whitish font-semibold hover:text-vivid-pink transition duration-500 w-full h-full flex items-center justify-center py-3 font-playfair group cursor-pointer'
+							className='dark:bg-bluish-black bg-whitish font-semibold group-hover:text-vivid-pink group-hover:translate-y-0.3 transition duration-500 w-full h-full flex items-center justify-center py-3 font-playfair cursor-pointer'
 						/>
 					</div>
 				</form>
-			</div>
+				{/* </div> */}
+			</motion.div>
 		</section>
 	);
 };
