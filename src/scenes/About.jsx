19e8eee3d80bcf195ca8d8f2c6a09f267c3888c5
sahoooii <saho.chu@gmail.com>
2025-03-04 16:39:54 +0900

    Add some space to improve readability, and add colors

--- src/scenes/About.jsx
+++ src/scenes/About.jsx
@@ -4,7 +4,7 @@ import LineGradient from 'components/LineGradient';
 
 const About = ({ isJp }) => {
 	const style =
-		'md:mt-10 mt-5 px-2 py-8 text-start tracking-wide text-base sm:text-xl md:text-lg leading-7 sm:leading-8 md:leading-9';
+		'md:mt-10 mt-5 px-4 py-8 text-start tracking-wide text-base sm:text-xl md:text-lg leading-7 sm:leading-8 md:leading-9 text-justify';
 
 	const styleTaglineBlue = 'text-light-blue font-bold';
 	const styleTaglineOrange = 'text-orange-500 font-bold';
@@ -37,16 +37,22 @@ const About = ({ isJp }) => {
 						<p className={`${style} font-jp`}>
 							数年間、独学でプログラミングを学び、常に新しい技術を吸収しながらスキルを磨いてきました。フロントエンドでは
 							React・Next.js・TypeScriptを中心に、バックエンドではNode.js
-							を活用し、データベースと連携したAPI
-							開発も手がけています。フルスタックの視点を活かしながら、特に使いやすいUIデザインにこだわり、ユーザーにとって快適な体験を提供できるよう心がけ、Webサービスの開発からデプロイまで取り組んでいます。
+							を活用し、データベースと連携したAPI 開発も手がけています。
+							<span className={`${styleTaglineBlue}`}>フルスタックの視点</span>
+							を活かしながら、特に
+							<span className={`${styleTaglineOrange}`}>
+								使いやすいUIデザイン
+							</span>
+							にこだわり、ユーザーにとって快適な体験を提供できるよう心がけ、Webサービスの開発からデプロイまで取り組んでいます。
+							<br />
 							<br />
 							長年のカスタマーサービス経験を通じて培った
 							<span className={`${styleTaglineBlue}`}>
-								「相手の考えを汲み取り行動する力」
+								"相手の考えを汲み取り行動する力"
 							</span>
 							を活かし、ユーザー目線で使いやすく洗練されたサービスを作ることを大切にしています。
 							<br />
-							将来的には、最適な出会いを提供する
+							将来的には、最高の出会いを提供する
 							<span className={`${styleTaglineOrange}`}>
 								マッチングアプリの開発
 							</span>
@@ -59,37 +65,50 @@ const About = ({ isJp }) => {
 								人と動物がより幸せに暮らせる仕組み
 							</span>
 							を作りたいと考えています。また、ハワイでの経験を活かし、
+							<span className={`${styleTaglineOrange}`}>"Deep Hawaii"</span>
+							をテーマに
 							<span className={`${styleTaglineBlue}`}>
-								ローカル視点で役立つ情報を提供するサービス
+								ローカル視点で役立つ情報を提供するプラットフォームサービス
 							</span>
-							にも挑戦したいです。
+							の構築にも挑戦したいです。
 							<br />
-							自分の経験を活かせる分野で、価値のあるサービスを生み出し、
+							<br />
 							<span className={`${styleTaglineOrange}`}>
 								どんなに難しい課題にも粘り強く取り組み、
 							</span>
+							自分の経験を活かせる分野で、価値のあるサービスを生み出し、
 							サービスを通じて人や動物の暮らしを支え、より良い未来を作れるエンジニアを目指しています。
 						</p>
 					) : (
 						<p className={`${style} font-opensans`}>
-							I have been learning programming for years, continuously exploring
-							new technologies. On the front end, I primarily work with React,
-							Next.js, and TypeScript, while on the back end, I develop APIs and
-							integrate databases using Node.js.
+							I have been learning programming for years, constantly exploring
+							new technologies.
 							<br />
-							Approaching development from a full-stack perspective, I
-							specialize in creating user-friendly UI designs that provide a
-							seamless and enjoyable experience for all users. I take ownership
-							of everything from development to deployment.
+							On the front end, I primarily work with React, Next.js, and
+							TypeScript, while on the back end, I develop APIs and integrate
+							databases using Node.js.
+							<br />
+							Approaching development from a
+							<span className={`${styleTaglineBlue}`}>
+								{' '}
+								full-stack perspective,{' '}
+							</span>
+							I specialize in creating
+							<span className={`${styleTaglineOrange}`}>
+								{' '}
+								user-friendly UI designs{' '}
+							</span>
+							that provide a seamless and enjoyable experience for all users. I
+							take ownership of everything from development to deployment.
+							<br />
 							<br />
 							With many years of customer service experience, I have learned to
 							<span className={`${styleTaglineBlue}`}>
 								{' '}
-								"Understand People's Needs And Take Action"{' '}
+								"Understand People's Needs And Take Action".{' '}
 							</span>
-							to support them. I value this skill and apply it to my
-							development, striving to create intuitive and well-designed
-							services.
+							I value this skill and apply it to my development, striving to
+							create intuitive and well-designed services.
 							<br />
 							Looking ahead, I have mapped out my career plan. I aspire to
 							<span className={`${styleTaglineBlue}`}>
@@ -104,14 +123,16 @@ const About = ({ isJp }) => {
 								a service dedicated to improving the lives of{' '}
 							</span>
 							<span className={`${styleTaglineOrange}`}> Dogs and Cats. </span>
-							Additionally, having lived in Hawaii for a couple of years, I want
-							to create a platform that provides tourists with
+							<br />
+							Having lived in Hawaii for a couple of years, I want to build a
+							platform offering tourists
 							<span className={`${styleTaglineBlue}`}>
 								{' '}
 								authentic, local insights into
 							</span>
 							<span className={`${styleTaglineOrange}`}> "Deep Hawaii." </span>
 							I aim to build meaningful services by leveraging my experiences.
+							<br />
 							<br />
 							<span className={`${styleTaglineOrange}`}>
 								{' '}
